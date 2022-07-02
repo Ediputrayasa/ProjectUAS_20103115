efb7522d4994dad6b3e8b2e323e102a54061c101
Ediputrayasa <101324946+Ediputrayasa@users.noreply.github.com>
2022-07-02 14:53:37 +0800

    Setor UAS

---
+++ ProMHS.java
@@ -0,0 +1,13 @@
+package promhs;
+
+/**
+ *
+ * @author I Kadek Edi Putrayasa 20103115
+ */
+public class ProMHS {
+
+    public static void main(String[] args) {
+       new frmSIM().setVisible(true);
+    }
+    
+}
